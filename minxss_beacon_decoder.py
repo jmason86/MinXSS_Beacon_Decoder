6c9a6c0edde3eb9fb5dbbe2dcfcaf9a9daa23af4
James Paul Mason <jmason86@gmail.com>
2017-02-27 16:54:17 -0700

    Now auto-detects serial ports and displays them in the combobox
    Can also connect and disconnect from the serial port

--- minxss_beacon_decoder.py
+++ minxss_beacon_decoder.py
@@ -11,49 +11,67 @@ from ui_mainWindow import Ui_MainWindow
 import connect_serial_decode_kiss
 from PySide import QtCore, QtGui
 import time, datetime
+from serial.tools import list_ports
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+        self.setupAvailablePorts()
         self.assignWidgets()
         self.setupOutputLog() # Log of serial data for user
         self.log = self.createLog() # Debug log
-        self.serialReadThread = SerialReadThread(self.readSerial, self.stopReadClicked)
+        self.serialReadThread = SerialReadThread(self.readSerial, self.stopRead)
         self.show()
     
+    def setupAvailablePorts(self):
+        self.comboBox_serialPort.clear()
+        listPortInfoObjects = list_ports.comports()
+        portNames = [x[0] for x in listPortInfoObjects]
+        self.comboBox_serialPort.addItems(portNames)
+    
     def assignWidgets(self):
-        self.button_connectSerial.clicked.connect(self.connecetSerialClicked)
-        self.button_startRead.clicked.connect(self.startReadClicked)
-        self.button_stopRead.clicked.connect(self.stopReadClicked)
+        self.actionConnectSerial.triggered.connect(self.connecetSerialClicked)
         self.checkBox_saveLog.stateChanged.connect(self.saveLogToggled)
     
     def connecetSerialClicked(self):
-        # Grab the port and baud rate from UI
-        port = self.textEdit_serialPort.toPlainText()
-        baudRate = self.textEdit_baudRate.toPlainText()
+        connectButtonText = str(self.actionConnectSerial.iconText())
+        if connectButtonText == "Connect":
+            self.log.info("Attempting to connect to serial port")
+            
+            # Update the GUI to diconnect button
+            self.actionConnectSerial.setText(QtGui.QApplication.translate("MainWindow", "Disconnect", None, QtGui.QApplication.UnicodeUTF8))
         
-        # Connect to the serial port and test that it is readable
-        connectedPort = connect_serial_decode_kiss.connect_serial_decode_kiss(port, baudRate, self.log)
-        portReadable = connectedPort.testRead()
-    
-        # If port is readable, store the reference to it and update the GUI
-        if portReadable:
-            self.connectedPort = connectedPort
-            self.label_connected.setText(QtGui.QApplication.translate("MainWindow", "Connected: Yes", None, QtGui.QApplication.UnicodeUTF8))
-            palette = QtGui.QPalette()
-            palette.setColor(QtGui.QPalette.Foreground, QColor(55, 195, 58)) # Green
-            self.label_connected.setPalette(palette)
-
-    def startReadClicked(self):
-        # Update the GUI reading toggle
-        self.label_reading.setText(QtGui.QApplication.translate("MainWindow", "Reading", None, QtGui.QApplication.UnicodeUTF8))
-        palette = QtGui.QPalette()
-        palette.setColor(QtGui.QPalette.Foreground, QColor(55, 195, 58)) # Green
-        self.label_reading.setPalette(palette)
+            # Grab the port and baud rate from UI
+            port = self.comboBox_serialPort.currentText()
+            baudRate = self.lineEdit_baudRate.text()
+            
+            # Connect to the serial port and test that it is readable
+            connectedPort = connect_serial_decode_kiss.connect_serial_decode_kiss(port, baudRate, self.log)
+            portReadable = connectedPort.testRead()
         
-        # Start the dedicated thread reading
-        self.serialReadThread.start()
+            # If port is readable, store the reference to it and start reading . Either way, update the GUI serial status
+            if portReadable:
+                # Store port in instance variable and start reading
+                self.connectedPort = connectedPort
+                self.serialReadThread.start()
+                
+                # Update GUI
+                self.label_serialStatus.setText(QtGui.QApplication.translate("MainWindow", "Reading", None, QtGui.QApplication.UnicodeUTF8))
+                palette = QtGui.QPalette()
+                palette.setColor(QtGui.QPalette.Foreground, QColor(55, 195, 58)) # Green
+                self.label_serialStatus.setPalette(palette)
+            else:
+                self.label_serialStatus.setText(QtGui.QApplication.translate("MainWindow", "Port test read failed", None, QtGui.QApplication.UnicodeUTF8))
+                palette = QtGui.QPalette()
+                palette.setColor(QtGui.QPalette.Foreground, QColor(242, 86, 77)) # Red
+                self.label_serialStatus.setPalette(palette)
+        else:
+            self.log.info("Attempting to disconnect from serial port")
+            
+            # Update the GUI to connect button
+            self.actionConnectSerial.setText(QtGui.QApplication.translate("MainWindow", "Connect", None, QtGui.QApplication.UnicodeUTF8))
+            self.stopRead()
         
     def readSerial(self):
         # Infinite loop to read the serial port and display the data in the GUI and optionally write to output file
@@ -68,18 +86,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                     serialOutputLog = open(self.serialOutputFilename, 'a') # append to existing file
                     serialOutputLog.write(formattedSerialData)
                     serialOutputLog.closed
-
-    def stopReadClicked(self):
-        # Update the GUI reading toggle
-        self.label_reading.setText(QtGui.QApplication.translate("MainWindow", "Not Reading", None, QtGui.QApplication.UnicodeUTF8))
+    
+    def stopRead(self):
+        self.connectedPort.close()
+    
+        # Update GUI
+        self.label_serialStatus.setText(QtGui.QApplication.translate("MainWindow", "Port closed", None, QtGui.QApplication.UnicodeUTF8))
         palette = QtGui.QPalette()
         palette.setColor(QtGui.QPalette.Foreground, QColor(242, 86, 77)) # Red
-        self.label_reading.setPalette(palette)
+        self.label_serialStatus.setPalette(palette)
 
     def saveLogToggled(self):
         if self.checkBox_saveLog.isChecked():
             # Create new log file
-            self.serialOutputFilename = 'JPM_serial_reader/output/' + datetime.datetime.now().isoformat() + '.txt'
+            self.serialOutputFilename = 'MinXSS_Beacon_Decoder/output/' + datetime.datetime.now().isoformat() + '.txt'
             with open(self.serialOutputFilename, 'w') as serialOutputLog:
                 # Update the GUI for the log file - is saving
                 self.label_savingToLogFile.setText("Saving to log file: " + self.serialOutputFilename)
@@ -95,9 +115,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.label_savingToLogFile.setPalette(palette)
 
     def setupOutputLog(self):
-        if not os.path.exists("JPM_serial_reader/output"):
-            os.makedirs("JPM_serial_reader/output")
-        self.serialOutputFilename = "JPM_serial_reader/output/" + datetime.datetime.now().isoformat() + ".txt"
+        if not os.path.exists("MinXSS_Beacon_Decoder/output"):
+            os.makedirs("MinXSS_Beacon_Decoder/output")
+        self.serialOutputFilename = "MinXSS_Beacon_Decoder/output/" + datetime.datetime.now().isoformat() + ".txt"
         with open(self.serialOutputFilename, 'w') as serialOutputLog:
             # Update the GUI for the log file - is saving
             self.label_savingToLogFile.setText("Saving to log file: " + self.serialOutputFilename)
@@ -107,10 +127,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         serialOutputLog.closed
 
     def createLog(self):
-        if not os.path.exists("JPM_serial_reader/log"):
-            os.makedirs("JPM_serial_reader/log")
+        if not os.path.exists("MinXSS_Beacon_Decoder/log"):
+            os.makedirs("MinXSS_Beacon_Decoder/log")
         log = logging.getLogger('serial_reader_debug')
-        handler = logging.FileHandler('JPM_serial_reader/log/serial_reader_debug.log')
+        handler = logging.FileHandler('MinXSS_Beacon_Decoder/log/minxss_beacon_decoder_debug.log')
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
         handler.setFormatter(formatter)
         log.addHandler(handler)
@@ -129,7 +149,7 @@ class SerialReadThread(QtCore.QThread):
 
 if __name__ == '__main__':
     log = logging.getLogger('serial_reader_debug')
-    handler = logging.FileHandler('JPM_serial_reader/log/serial_reader_debug.log')
+    handler = logging.FileHandler('MinXSS_Beacon_Decoder/log/minxss_beacon_decoder_debug.log')
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
     handler.setFormatter(formatter)
     log.addHandler(handler)
