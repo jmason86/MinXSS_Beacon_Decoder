6721ab8b4d9021ee562fb9a48e780e88d5f76986
James Paul Mason <jmason86@gmail.com>
2018-09-26 22:26:42 -0400

    Only upload files if they aren't empty

--- file_upload.py
+++ file_upload.py
@@ -2,17 +2,17 @@
 __authors__ = "James Paul Mason"
 __contact__ = "jmason86@gmail.com"
 
+import os
 import requests
 
 
 def upload(filename, log):
-    # Server settings
     url = 'http://lasp.colorado.edu/minxss/beacon/fileupload.php'
-    
-    # Access the file
+
     file_to_send = {'filename': (filename, open(filename, 'rb'))}
-
-    # Send the file
-    r = requests.post(url, files=file_to_send)
-
-    log.info(r.text)
+    if os.path.getsize(filename) > 0:
+        r = requests.post(url, files=file_to_send)
+        log.info('Uploading data.')
+        log.info(r.text)
+    else:
+        log.info('Not uploading because data file is empty.')
