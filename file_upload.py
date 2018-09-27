5ff893dddd8ae687e7b2f4010982bf355f40137b
James Paul Mason <jmason86@gmail.com>
2018-09-26 21:53:04 -0400

    Refactor for PEP8 use of underscores

--- file_upload.py
+++ file_upload.py
@@ -4,14 +4,15 @@ __contact__ = "jmason86@gmail.com"
 
 import requests
 
+
 def upload(filename, log):
     # Server settings
     url = 'http://lasp.colorado.edu/minxss/beacon/fileupload.php'
     
     # Access the file
-    fileToSend = {'filename': (filename, open(filename, 'rb'))}
+    file_to_send = {'filename': (filename, open(filename, 'rb'))}
 
     # Send the file
-    r = requests.post(url, files = fileToSend)
+    r = requests.post(url, files=file_to_send)
 
     log.info(r.text)
