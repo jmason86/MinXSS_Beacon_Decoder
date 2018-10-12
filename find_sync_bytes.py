48eec35fd7386069f7a532946b4d077caed479e3
James Paul Mason <jmason86@gmail.com>
2018-10-11 20:47:42 -0400

    Refactor name to be more fitting for scope

--- find_sync_bytes.py
+++ find_sync_bytes.py
@@ -4,11 +4,11 @@ class FindSyncBytes:
         self.start_sync_bytes = bytearray([0x08, 0x19])  # Beacon housekeeping data
         self.stop_sync_bytes = bytearray([0xa5, 0xa5])
 
-    def find_log_sync_start_index(self, buffered_serial_data):
-        return bytearray(buffered_serial_data).find(self.log_sync_bytes)
+    def find_log_sync_start_index(self, packets):
+        return bytearray(packets).find(self.log_sync_bytes)
 
-    def find_sync_start_index(self, buffered_serial_data):
-        return bytearray(buffered_serial_data).find(self.start_sync_bytes)
+    def find_sync_start_index(self, packets):
+        return bytearray(packets).find(self.start_sync_bytes)
 
-    def find_sync_stop_index(self, buffered_serial_data):
-        return bytearray(buffered_serial_data).find(self.stop_sync_bytes)
+    def find_sync_stop_index(self, packets):
+        return bytearray(packets).find(self.stop_sync_bytes)
